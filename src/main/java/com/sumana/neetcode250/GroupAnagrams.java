47eb6c54dc0049d444ffe3381e372cac2d269403
colombia9503 <santi950312@gmail.com>
2025-09-12 13:08:13 -0500

    Group anagrams solution implemented

--- src/main/java/com/sumana/neetcode250/GroupAnagrams.java
+++ src/main/java/com/sumana/neetcode250/GroupAnagrams.java
@@ -1,14 +1,32 @@
 package com.sumana.neetcode250;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GroupAnagrams {
     public static List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
 
+        Map<String, List<String>> resultMap = new HashMap<>();
+
+        for (String str : strs) {
+            String sortedString = sortString(str);
+            resultMap.putIfAbsent(sortedString, new ArrayList<>());
+            resultMap.get(sortedString).add(str);
+        }
+
+        return new ArrayList<>(resultMap.values());
+    }
+
+    private static String sortString(String str) {
+        char[] chArr = str.toCharArray();
+        Arrays.sort(chArr);
+        return String.valueOf(chArr);
     }
 
     public static void main(String[] args) {
-
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
     }
 }
